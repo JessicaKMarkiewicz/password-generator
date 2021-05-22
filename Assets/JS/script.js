457789d10074e79183bee40a33b69bb1576c481d
JessicaAKelley <kelleyajessica@gmail.com>
2021-05-22 07:28:36 -0700

    User prompts for password length

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,29 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericalChar = "0123456789";
-var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var lowercaseCharac = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharac = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericalCharac = "0123456789";
+var specialCharac = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 function generatePassword() {
 
     var password = "";
-    var passwordChar = "";
+    var passwordCharac = "";
     // Create prompt to select password length
+    var passwordLengthUser = prompt("How many characters would you like your password to be? Password MUST be between 8-128 characters.");
+    passwordLengthUser = parseInt(passwordLengthUser);
+
+    if (passwordLengthUser < 8) {
+        alert("Sorry, invalid. Password must be at least eight characters.");
+        return "";
+    }
     
+    if (passwordLengthUser > 128) {
+        alert("Sorry, invalid. Password must not exceed 128 characters.");
+        return "";
+    }
+
 }
 
 // Write password to the #password input
@@ -24,4 +36,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
